8fe369e0635293ebd9bc4fef5c9c83cd594f4cec
adityaraj067 <adityarajpandey62003@gmail.com>
2025-06-19 14:18:48 +0530

    Initial commit

--- encapsulation.py
+++ encapsulation.py
@@ -1,11 +1,17 @@
-class Students:
-    # public method
-    def __init__(self,name,age,marks):
-        self.name = name
-        self.age= age
-        self.marks = marks
-def display(self):
-    print(f"Age of student is {self.age}")
+# class Students:
+#     # public method
+#     def __init__(self,name,age,marks):
+#         self.name = name
+#         self.age= age
+#         self.marks = marks
+# def display(self):
+#     print(f"Age of student is {self.age}")
 
-def get_marks(self):
-    print(f)
+# def get_marks(self):
+#     print(f)
+
+
+
+
+
+
