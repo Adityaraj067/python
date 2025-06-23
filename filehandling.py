a2268ed4c9fce29bccd07b704605e0603d751096
adityaraj067 <adityarajpandey62003@gmail.com>
2025-06-23 09:56:50 +0530

    modified file

--- filehandling.py
+++ filehandling.py
@@ -3,9 +3,16 @@
 # print(write_data)
 # f.close()
 
-f = open("test.txt",'r')
-read_data = f.readlines()
-for i in read_data:
-    print(i)  
+# f = open("test.txt",'r')
+# read_data = f.readlines()
+# for i in read_data:
+#     print(i)  
+# print(read_data)
+# f.close()
+
+with open("test.txt",'w') as v:
+    v.write("jai shree ram")
+f= open("test.txt")
+read_data = f.read()
 print(read_data)
 f.close()
